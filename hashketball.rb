884a04b501bb6899c2336b4bcdaf05e0e9b2fd7a
Kevin Park <kevin.park07@yahoo.com>
2020-07-15 17:14:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
 def game_hash
-  final_hash = {
-  :home => {
-    :team_name => "",
-    :colors => [],
-    :players => [
-      {:player_name =>""},
-      {
-      ]
-  },
-  :away => {}
-}
+ game_hash = {}
+end
