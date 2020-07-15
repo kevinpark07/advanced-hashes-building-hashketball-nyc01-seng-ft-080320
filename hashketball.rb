82639ffd76d61e0ddb661fe00008fdb199cee36f
Kevin Park <kevin.park07@yahoo.com>
2020-07-15 17:38:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,19 +3,19 @@ require 'pry'
 def game_hash
   game_hash = {
     :home => {
-      :team_name => {"Brooklyn Nets"},
-      :colors => {"Black", "White"},
+      :team_name => {},
+      :colors => {},
       :players => {
         [{
-          :player_name => "Alan Anderson",
-          :number => "0",
-          :shoe => "16",
-          :points => "22",
-          :rebounds =>"12",
-          :assists =>"12",
-          :steals =>"3",
-          :blocks =>"1",
-          :slam_dunks => "1"
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
         }
           
           ]
